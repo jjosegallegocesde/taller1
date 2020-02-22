b18d13583c41f3be576361104845a98f049379e9
jjosegallegocesde <61057518+jjosegallegocesde@users.noreply.github.com>
2020-02-21 21:04:03 -0500

    primer ejercicio

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 
-echo("Hola mundo");
+//Ejercicio 
+
+echo("Taller 1 elaborado por Juan Gallego <br>");
+echo("Punto 1");
+
+$numero1=500;
+$numero2=1000;
+
+$suma=$numero1+$numero2;
+$resta=$numero1-$numero2;
+$multiplicacion=$numero1*$numero2;
+$division=$numero1/$numero2;
+
+echo("<br>La suma de:".$numero1."+".$numero2."=".$suma);
+
+
 
 ?>
